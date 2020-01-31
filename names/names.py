6344d8cc82567e480004d5ef8ccce0a070ce8eaa
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-01-31 16:14:58 -0500

    finished MVP

--- names/names.py
+++ names/names.py
@@ -10,11 +10,28 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+duplicates = [name for name in names_1 if name is name in names_2] #ALL HAIL LIST COMPREHENSIONS! :D
+# --------------------------------------------------------
+# INITIAL PROBLEM TO IMPROVE - RUN TIME COMPLEXITY = QUADRATIC TIME!
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+# # -------------------------------------------------------- SO MUCH WORST XD
+# duplicates = []
+# def find_dups(names1, names2, first_index=0, second_index=0):
+#     done = False
+#     while not done:
+#         if names1[first_index] is names2[second_index]:
+#             duplicates.append(names1[first_index])
+#         second_index += 1
+#         if names2[second_index] is names2[-1]:
+#             second_index = 0
+#             first_index += 1
+#         if names1[first_index] is names1[-1]:
+#             done = True
+#     return duplicates
+# find_dups(names_1, names_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
